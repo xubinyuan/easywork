4634501a420a6c0df0706389046c1749a42894ea
xubinyuan <1182577696@qq.com>
2017-05-11 15:51:46 +0800

    Print "Hello World!"

---
+++ src/org/xubinyuan/java/java.java
@@ -0,0 +1,10 @@
+package org.xubinyuan.java;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
